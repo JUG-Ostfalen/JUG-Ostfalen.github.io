f3bf2a29ed6bbe8c68aee9238c2226075a9675d2
Gerrit Meier <meistermeier@gmail.com>
2017-04-15 10:42:20 +0200

    update newsletter converter to accept date
    if provided the article will be get the timestamp of the specified date in dd.mm.YYYY

--- _newsletter/convert_newsletter.rb
+++ _newsletter/convert_newsletter.rb
@@ -1,6 +1,34 @@
 #!/usr/bin/env ruby
+require 'time'
 
+begin
+today = Time.strptime(ARGV[0], "%d.%m.%Y")
+rescue
 today = Time.new
+end
+
+def toMonth(number)
+	case number
+	when 1 then "Januar"
+	when 2 then "Februar"
+	when 3 then "März"
+	when 4 then "April"
+	when 5 then "Mai"
+	when 6 then "Juni"
+	when 7 then "Juli"
+	when 8 then "August"
+	when 9 then "September"
+	when 10 then "Oktober"
+	when 11 then "November"
+	when 12 then "Dezember"
+	end
+end
+
+if today == nil
+
+end
+
+puts today
 
 output = File.open("../_posts/#{today.strftime("%Y/")}/#{today.strftime("%Y-%m-%d")}-iJUG-Newsletter.md", 'w') { |f|
 
@@ -8,14 +36,17 @@ newsletter_link = nil;
 
 File.open('newsletter.txt').each_with_index do |line, i|
 	if i == 0 # newsletter title
-		header = 
+		title = "iJUG-Newsletter #{toMonth(today.month)} #{today.strftime("%Y")}"
+		header =
 %Q(---
 layout: post
-title: "#{line.chomp}"
+title: "#{title}"
 date:   #{today.strftime("%F")}
 categories: article
 author: ijug
----)
+---
+Dies ist der monatliche Newsletter des iJUG mit Informationen zu Konferenzen und Veranstaltungen.
+)
 		f.write(header)
 	elsif i==2
 		newsletter_link = line.chomp
@@ -33,4 +64,3 @@ author: ijug
 end
 	f.write("[Link zum Newsletter](#{newsletter_link})\n");
 }
-
